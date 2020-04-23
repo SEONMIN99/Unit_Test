a2a01a77c99d36b70793a27b3ecd3faafdd07d7c
SEONMIN99 <seonmin99@naver.com>
2020-04-23 17:29:28 +0900

    Unit Test

--- RobotTest.java
+++ RobotTest.java
@@ -39,4 +39,17 @@ public class RobotTest
     public void tearDown()
     {
     }
+
+    @Test
+    public void CheckSpeed()
+    {
+        Robot robot1 = new Robot();
+        robot1.Robotwalk("녹색", 20);
+        assertEquals(20.0, robot1.getSpeed(), 0.0);
+        robot1.Robotwalk("노란색", 20);
+        assertEquals(24.0, robot1.getSpeed(), 0.0);
+        robot1.Robotwalk("빨간색", 20);
+        assertEquals(0.0, robot1.getSpeed(), 0.0);
+    }
 }
+
