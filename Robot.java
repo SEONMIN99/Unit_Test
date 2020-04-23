e84564f418652cada1777b10d893ff11c3e9f902
sujeong <tnwjd3730@naver.com>
2020-04-23 17:13:03 +0900

    Create RobotWalk() and getSpeed()

--- Robot.java
+++ Robot.java
@@ -7,5 +7,26 @@
  */
 public class Robot
 {
+    private double speed;
+    public void Robotwalk(String signal, double speed){
+        if (signal == "녹색")
+        {
+            this.speed = speed;
+        }
+        else
+        {
+            if(signal == "노란색")
+            {
+                this.speed = speed * 1.2;
+            }
+            else        
+            {
+                speed = 0;
+            }
+        }
+    }
     
+    public double getSpeed(){
+        return this.speed;   
+    }
 }
