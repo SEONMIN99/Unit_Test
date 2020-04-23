4f3797a303cf99cb0f41080e47d54fc86725266e
sujeong <tnwjd3730@naver.com>
2020-04-23 17:13:58 +0900

    Create RobotTest Class

---
+++ RobotTest.java
@@ -0,0 +1,42 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class RobotTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class RobotTest
+{
+    /**
+     * Default constructor for test class RobotTest
+     */
+    public RobotTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
